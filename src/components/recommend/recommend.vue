297a0d63e66bad843e9b92e8885d4f67902c4c92
patient <1923407743@qq.com>
2018-11-08 14:11:42 +0800

    config api

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
+  <div class="recommend">
     recommend
   </div>
 </template>
 
 <script>
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
 export default {
-  name: ''
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
 }
 </script>
 
