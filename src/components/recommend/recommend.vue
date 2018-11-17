c164051dcde6532e822b2b905defb3627843eef2
patient <1923407743@qq.com>
2018-11-17 17:49:24 +0800

    recommend style recommend list

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -9,6 +9,20 @@
         </div>
       </slider>
     </div>
+    <div class="recommend-list">
+      <h1 class="list-title">热门歌单推荐</h1>
+      <ul>
+        <li v-for="(item, index) in discLikst" :key="index" class="item">
+          <div class="icon">
+            <img width="60" height="60" :src="item.imgurl">
+          </div>
+          <div class="text">
+            <h2 class="name" v-html="item.creator.name"></h2>
+            <p class="desc" v-html="item.dissname"></p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,7 +38,8 @@ export default {
   },
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discLikst: []
     }
   },
   methods: {
@@ -38,7 +53,7 @@ export default {
     _getDiscLikst() {
       getDiscLikst().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data)
+          this.discLikst = res.data.list
         }
       })
     }
@@ -50,5 +65,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+@import '~common/stylus/variable'
+.recommend-list
+  .list-title
+    height: 65px
+    line-height: 65px
+    text-align: center
+    font-size: $font-size-medium
+    color: $color-theme
+  .item
+    display: flex
+    box-sizing: border-box
+    align-items: center
+    padding: 0 20px 20px 20px
+    .icon
+      flex: 0 0 60px
+      width: 60px
+      padding-right: 20px
+    .text
+      display: flex
+      flex-direction: column
+      justify-content: center
+      line-height: 20px
+      overflow: hidden
+      font-size: $font-size-medium
+      .name
+        margin-bottom: 10px
+      .desc
+        color: $color-text-d
 </style>
