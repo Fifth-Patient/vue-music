da0a950c0c96b289ceb8536521d8da963e2647d3
patient <1923407743@qq.com>
2018-11-22 16:00:12 +0800

    singer

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,3 +11,12 @@ export const addClass = (el, className) => {
   newClass.push(className)
   el.className = newClass.join(' ')
 }
+
+export const getData = (el, name, val) => {
+  const prefix = 'data-'
+  if (val) {
+    return el.setAttribute(prefix + name, val)
+  } else {
+    return el.getAttribute(prefix + name)
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
 import { ERR_OK } from 'api/config'
 import { getSingerList } from 'api/singer'
 import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LENGTH = 10
@@ -25,8 +26,7 @@ export default {
     _getSingerList() {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this._normalizeSinger(this.singers))
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
     },
@@ -69,9 +69,11 @@ export default {
       ret.sort((a, b) => {
         return a.title.charCodeAt(0) - b.title.charCodeAt(0)
       })
-      console.log(hot)
       return hot.concat(ret)
     }
+  },
+  components: {
+    ListView
   }
 }
 </script>
