21cdc87037f0de11a924226e8b370e469017d0b9
patient <1923407743@qq.com>
2018-11-29 14:50:35 +0800

    tempurl and url

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -20,7 +20,8 @@ export const createSong = (musicData, vkey) => {
     ablum: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?vkey=${vkey}&guid=342833894&uin=0&fromtag=3&=r18486347919424384`
+    url: `http://183.232.248.19/amobile.music.tc.qq.com/C4000039MnYb0qxYhV.m4a?guid=342833894&vkey=2A7E84811DB4BE4417EAF4964A9A59FC3E25D9A04E80E7A6831E8FED09DD7464C1B7A9494256FF72EEB41A944A52C340E86B6B1314DE1C8B&uin=0&fromtag=66`,
+    tempurl: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?vkey=${vkey}&guid=342833894&uin=0&fromtag=3&=r18486347919424384`
   })
 }
 
