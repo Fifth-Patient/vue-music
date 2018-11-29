eb953a73c3e607ffb414bbc9b0ea00b5c80d141c
patient <1923407743@qq.com>
2018-11-29 21:08:30 +0800

    normal player, mini player base animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,48 +1,52 @@
 <template>
   <div class="player" v-show="playList.length>0">
-    <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition name="normal">
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image">
         </div>
-        <div class="title" v-html="currentSong.name"></div>
-        <div class="subtitle" v-html="currentSong.singer"></div>
-      </div>
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image">
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <div class="title" v-html="currentSong.name"></div>
+          <div class="subtitle" v-html="currentSong.singer"></div>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image">
+              </div>
             </div>
           </div>
+          <div class="middle-r"></div>
         </div>
-        <div class="middle-r"></div>
-      </div>
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon i-left"><i class="icon-random"></i></div>
-          <div class="icon i-left"><i class="icon-prev"></i></div>
-          <div class="icon i-center"><i class="icon-play"></i></div>
-          <div class="icon i-right"><i class="icon-next"></i></div>
-          <div class="icon i-right"><i class="icon-not-favorite"></i></div>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left"><i class="icon-random"></i></div>
+            <div class="icon i-left"><i class="icon-prev"></i></div>
+            <div class="icon i-center"><i class="icon-play"></i></div>
+            <div class="icon i-right"><i class="icon-next"></i></div>
+            <div class="icon i-right"><i class="icon-not-favorite"></i></div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="mini-player" v-show="!fullScreen" @click="open">
-      <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img width="40" height="40" :src="currentSong.image">
+        </div>
+        <div class="text">
+          <div class="name" v-html="currentSong.name"></div>
+          <div class="desc" v-html="currentSong.singer"></div>
+        </div>
+        <div class="control">
+          <div class="icon-playlist"></div>
+        </div>
       </div>
-      <div class="text">
-        <div class="name" v-html="currentSong.name"></div>
-        <div class="desc" v-html="currentSong.singer"></div>
-      </div>
-      <div class="control">
-        <div class="icon-playlist"></div>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -237,6 +241,16 @@ export default {
         transform: translate3d(0, -100px, 0)
       .bottom
         transform: translate3d(0, 100px, 0)
+    &.normal-enter-active, &.normal-leave-active
+      transition: all .4s
+      .top, .bottom
+        transition: all .4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+    &.normal-enter, &.normal-leave-to
+      opacity: 0
+      .top
+        transform: translate3d(0, -100px, 0)
+      .bottom
+        transform: translate3d(0, 100px, 0)
   .mini-player
     display: flex
     align-items: center
@@ -281,4 +295,8 @@ export default {
         position: absolute
         left: 0
         top: 0
+    &.mini-enter-active, &.mini-leave-active
+      transition: all .4s
+    &.mini-enter, &.mini-leave-to
+      opacity: 0
 </style>
