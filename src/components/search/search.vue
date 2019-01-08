5cbb46cbe0621d55aa70716d4b3cce6180cee6c8
patient <1923407743@qq.com>
2019-01-08 15:15:55 +0800

    api/set Hot Key

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    <search-box></search-box>
+  <div class="search">
+    <div class="serach-wrapper">
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li @click="addQuery(item.k)" class="item" v-for="item in hotKey" :key="item.index">
+                <span>{{item.k}}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import SearchBox from 'components/search-box/search-box'
-
+import { getHotKey } from 'api/search'
+import { ERR_OK } from 'api/config'
 export default {
+  created() {
+    this._getHotKey()
+  },
+  data() {
+    return {
+      hotKey: []
+    }
+  },
+  methods: {
+    _getHotKey() {
+      getHotKey().then(res => {
+        if (res.code === ERR_OK) {
+          this.hotKey = res.data.hotkey.slice(0, 10)
+        }
+      })
+    },
+    addQuery(query) {
+      this.$refs.searchBox.setQuery(query)
+    }
+  },
   components: {
     SearchBox
   }
@@ -15,5 +52,52 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+@import '~common/stylus/variable'
+@import '~common/stylus/mixin'
+.search
+  .search-box-wrapper
+    margin: 20px
+  .shortcut-wrapper
+    position: fixed
+    top: 178px
+    bottom: 0
+    width: 100%
+    .shortcut
+      height: 100%
+      overflow: hidden
+      .hot-key
+        margin: 0 20px 20px 20px
+        .title
+          margin-bottom: 20px
+          font-size: $font-size-medium
+          color: $color-text-l
+        .item
+          display: inline-block
+          padding: 5px 10px
+          margin: 0 20px 10px 0
+          border-radius: 6px
+          background: $color-highlight-background
+          font-size: $font-size-medium
+          color: $color-text-d
+      .search-history
+        position: relative
+        margin: 0 20px
+        .title
+          display: flex
+          align-items: center
+          height: 40px
+          font-size: $font-size-medium
+          color: $color-text-l
+          .text
+            flex: 1
+          .clear
+            extend-click()
+            .icon-clear
+              font-size: $font-size-medium
+              color: $color-text-d
+  .search-result
+    position: fixed
+    width: 100%
+    top: 178px
+    bottom: 0
 </style>
