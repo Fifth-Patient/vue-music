2a7e02a0b2f037d65b0d8669a4a9851970ca5d18
patient <1923407743@qq.com>
2019-03-11 17:36:52 +0800

    vue router lazy-loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from 'components/rank/rank'
-import Recommend from 'components/recommend/recommend'
-import Search from 'components/search/search'
-import Singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
+
+const Recommend = resolve => {
+  import('components/recommend/recommend').then(recommend => {
+    resolve(recommend)
+  })
+}
+
+const Rank = reslove => {
+  import('components/rank/rank').then(module => {
+    reslove(module)
+  })
+}
+const Search = reslove => {
+  import('components/search/search').then(module => {
+    reslove(module)
+  })
+}
+
+const Singer = reslove => {
+  import('components/singer/singer').then(module => {
+    reslove(module)
+  })
+}
+
+const SingerDetail = reslove => {
+  import('components/singer-detail/singer-detail').then(module => {
+    reslove(module)
+  })
+}
+
+const Disc = reslove => {
+  import('components/disc/disc').then(module => {
+    reslove(module)
+  })
+}
+
+const TopList = reslove => {
+  import('components/top-list/top-list').then(module => {
+    reslove(module)
+  })
+}
+
+const UserCenter = reslove => {
+  import('components/user-center/user-center').then(module => {
+    reslove(module)
+  })
+}
 
 export default new Router({
   routes: [
